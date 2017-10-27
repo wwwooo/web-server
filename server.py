9e681b234a9a81a8fa455bbefd853b2206b18e12
wwwooo <malinovsvska@gmail.com>
2017-10-27 23:17:10 +0300

    update server

--- server.py
+++ server.py
@@ -1,50 +1,50 @@
 import socket
-import threading
-import time
 
 
 def response(req):
-    arr_req = req.decode().split('\n')
-    arr_req_line = arr_req[0].split(' ')
-    resource = arr_req_line[1]
-    headers = ''
+    # print(req)
+    if req:
+        arr_req = req.decode().split('\n')
+        arr_req_line = arr_req[0].split(' ')
+        resource = arr_req_line[1]
+        headers = ''
 
-    if resource == '/':
-        resource = '/index.html'
+        if resource == '/':
+            resource = '/index.html'
 
-    with open('.' + resource, 'rb') as file:
-        message_body = file.read()
+        with open('.' + resource, 'rb') as file:
+            message_body = file.read()
 
-    if resource.endswith('.css'):
-        headers = 'Content-Type: text/css'
-    elif resource.endswith('.ico'):
-        headers = 'Content-Type: image/x-icon'
+        if resource.endswith('.css'):
+            headers = 'Content-Type: text/css'
+        elif resource.endswith('.ico'):
+            headers = 'Content-Type: image/x-icon'
 
-    return bytes('HTTP/1.1 200 OK\n' + headers + '\n\n', encoding='utf-8') + message_body
-
-
-def server():
-    try:
-        while True:
-            conn, addr = sock.accept()
-            conn.send(response(conn.recv(1024)))
-            conn.close()
-    except OSError:
-        print('pressed Ctrl-C')
-
+        return bytes('HTTP/1.1 200 OK\n' + headers + '\n\n', encoding='utf-8') + message_body
+    return b'blalala'
 
 sock = socket.socket()
 sock.bind(('localhost', 80))
 sock.listen()
-
-t = threading.Thread(target=server)
-t.start()
+sock.settimeout(1)
 
 while True:
     try:
-        time.sleep(1)
+        try:
+            conn, addr = sock.accept()
+            conn.settimeout(1)
+            data = b''
+            while data.find(b'\r\n\r\n') == -1:
+                a = conn.recv(1024)
+                if a:
+                    data += a
+                else:
+                    break
+            conn.sendall(response(data))
+            conn.close()
+        except socket.timeout:
+            pass
     except KeyboardInterrupt:
+        print('pressed Ctrl-C')
         sock.close()
         break
-
-t.join()
