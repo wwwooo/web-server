1156382ca904cc17c5802b8c92e7758bd996e82b
wwwooo <malinovsvska@gmail.com>
2017-10-28 20:22:15 +0300

    done query separation

--- server.py
+++ server.py
@@ -1,27 +1,60 @@
 import socket
 
 
-def response(req):
-    # print(req)
-    if req:
-        arr_req = req.decode().split('\n')
-        arr_req_line = arr_req[0].split(' ')
-        resource = arr_req_line[1]
-        headers = ''
+def response(req_line, h, body):
+    req_line = req_line.decode().split(' ')
+    resource = req_line[1]
+    headers = ''
 
-        if resource == '/':
-            resource = '/index.html'
+    if resource == '/':
+        resource = '/index.html'
 
-        with open('.' + resource, 'rb') as file:
-            message_body = file.read()
+    with open('.' + resource, 'rb') as file:
+        message_body = file.read()
 
-        if resource.endswith('.css'):
-            headers = 'Content-Type: text/css'
-        elif resource.endswith('.ico'):
-            headers = 'Content-Type: image/x-icon'
+    if resource.endswith('.css'):
+        headers = 'Content-Type: text/css'
+    elif resource.endswith('.ico'):
+        headers = 'Content-Type: image/x-icon'
 
-        return bytes('HTTP/1.1 200 OK\n' + headers + '\n\n', encoding='utf-8') + message_body
-    return b'blalala'
+    return bytes('HTTP/1.1 200 OK\n' + headers + '\n\n', encoding='utf-8') + message_body
+
+
+def arr_into_dic(arr):
+    dic = {}
+    for elem in arr:
+        mas = elem.split(b': ')
+        dic[mas[0]] = mas[1]
+    return dic
+
+
+def readData(s):
+    data = b''
+    while b'\r\n' not in data:
+        data += s.recv(1024)
+    i = data.find(b'\r\n')
+    start_line = data[:i]
+    print(start_line)
+
+    data = data[i + 2:]
+    while b'\r\n\r\n' not in data:
+        data += s.recv(1024)
+    i = data.find(b'\r\n\r\n')
+    headers = data[:i]
+    arr = headers.split(b'\r\n')
+    headers = arr_into_dic(arr)
+    print(headers)
+
+    body = data[i + 4:]
+    if b'Content-Length' in headers:
+        cont_l = headers[b'Content-Length']
+        numb = int(cont_l) - len(body)
+        if numb >= 0:
+            body += s.recv(numb)
+        print(body)
+    print('-----------------')
+
+    return start_line, headers, body
 
 sock = socket.socket()
 sock.bind(('localhost', 80))
@@ -33,14 +66,8 @@ while True:
         try:
             conn, addr = sock.accept()
             conn.settimeout(1)
-            data = b''
-            while data.find(b'\r\n\r\n') == -1:
-                a = conn.recv(1024)
-                if a:
-                    data += a
-                else:
-                    break
-            conn.sendall(response(data))
+            l, h, b = readData(conn)
+            conn.sendall(response(l, h, b))
             conn.close()
         except socket.timeout:
             pass
