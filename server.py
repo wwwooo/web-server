76d6c09e96beadafe3a82e64205baa2ec54f57fc
wwwooo <malinovsvska@gmail.com>
2017-10-29 15:04:28 +0200

    add success:html ans css

--- server.py
+++ server.py
@@ -2,12 +2,17 @@ import socket
 
 
 def response(req_line, h, body):
-    req_line = req_line.decode().split(' ')
+    req_line = req_line.split(' ')
     resource = req_line[1]
     headers = ''
 
-    if resource == '/':
-        resource = '/index.html'
+    if 'Content-Length' not in h:
+        if resource == '/':
+            resource = '/index.html'
+    else:
+        if resource == '/':
+            resource = '/success.html'
+
 
     with open('.' + resource, 'rb') as file:
         message_body = file.read()
@@ -20,40 +25,20 @@ def response(req_line, h, body):
     return bytes('HTTP/1.1 200 OK\n' + headers + '\n\n', encoding='utf-8') + message_body
 
 
-def arr_into_dic(arr):
-    dic = {}
-    for elem in arr:
-        mas = elem.split(b': ')
-        dic[mas[0]] = mas[1]
-    return dic
-
-
 def readData(s):
-    data = b''
-    while b'\r\n' not in data:
-        data += s.recv(1024)
-    i = data.find(b'\r\n')
-    start_line = data[:i]
-    print(start_line)
-
-    data = data[i + 2:]
-    while b'\r\n\r\n' not in data:
-        data += s.recv(1024)
-    i = data.find(b'\r\n\r\n')
-    headers = data[:i]
-    arr = headers.split(b'\r\n')
-    headers = arr_into_dic(arr)
-    print(headers)
-
-    body = data[i + 4:]
-    if b'Content-Length' in headers:
-        cont_l = headers[b'Content-Length']
-        numb = int(cont_l) - len(body)
-        if numb >= 0:
-            body += s.recv(numb)
-        print(body)
-    print('-----------------')
-
+    data = ''
+    while '\r\n\r\n' not in data:
+        data += s.recv(1024).decode('utf-8')
+    head, body = data.split('\r\n\r\n')
+    parts = head.split('\r\n')
+    start_line = parts[0]
+    # headers = {h[0]: h[1].strip() for h in (p.split(':') for p in parts[1:])}
+    headers = {}
+    for p in parts[1:]:
+        h = p.split(':')
+        headers[h[0]] = h[1].strip()
+    if 'Content-Length' in headers:
+        body += s.recv(int(headers['Content-Length']) - len(body)).decode('utf-8')
     return start_line, headers, body
 
 sock = socket.socket()
@@ -67,6 +52,8 @@ while True:
             conn, addr = sock.accept()
             conn.settimeout(1)
             l, h, b = readData(conn)
+            print(l, h, b)
+            print('**********************')
             conn.sendall(response(l, h, b))
             conn.close()
         except socket.timeout:
